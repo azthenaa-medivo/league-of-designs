bdb527dedd35bea422d30bb7f14db2e30a043ed6
Artemys <jacques.gegout@telecomnancy.net>
2015-09-02 17:45:38 +0200

    Crontab file update

--- server/api/lod_update.sh
+++ server/api/lod_update.sh
@@ -1,4 +1,4 @@
-echo [`date +"%m-%d-%y %T"`] Red Posts update
 workon env1
-python ~/server/api/red_api.py r
-sh ~/server/api/postimport.sh
+python /root/league-of-designs/server/api/red_api.py r
+sh /root/league-of-designs/server/api/postimport.sh
+echo [`date +"%m-%d-%y %T"`] Red Posts Update Okay >> ~/crontab.log
