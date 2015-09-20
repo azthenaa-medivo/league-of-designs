1c62aa0a33733c234319afe2cfc180b5e4cf035e
Artemys <jacques.gegout@telecomnancy.net>
2015-09-20 16:44:25 +0200

    Added a static_models_data context processor to have recurrent static data available at all times in templates.

--- lod_django/utilities/context_processors.py
+++ lod_django/utilities/context_processors.py
@@ -1,6 +1,10 @@
+from app_lod.models import REGIONS, GLORIOUS_SECTIONS
 from app_database.consumer import Consumer
 
 consumer = Consumer()
 
 def champions_list_data(request):
     return {'champions': [c for c in consumer.get_champions()]}
+
+def static_models_data(request):
+    return {'regions': zip(REGIONS, REGIONS), 'glorious_sections': GLORIOUS_SECTIONS}
