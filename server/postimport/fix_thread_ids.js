48362bd39cbee2ccc84fae067c59bebcd28262d9
Artemys <jacques.gegout@telecomnancy.net>
2015-11-11 18:20:49 +0100

    Removed a pesky print.

--- server/postimport/fix_thread_ids.js
+++ server/postimport/fix_thread_ids.js
@@ -1,7 +1,6 @@
 db.mr_reds.find({'thread_id': {'$exists': 0}}).forEach(function(res) {
     if (res['post_id'] != undefined)
     {
-        print(res['post_id']);
         var thread_id = res['post_id'].split(',')[0];
         var re = new RegExp('(^'+thread_id+',|^'+thread_id+'$)');
         db.mr_reds.update({'post_id': {'$regex': re}}, {'$set': {'thread_id': thread_id}}, {'multi': true});
