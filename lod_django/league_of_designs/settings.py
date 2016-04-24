b9f7e0070adb1fc8ade8a0a420f18b9d60b2146e
Artemys <jacques.gegout@telecomnancy.net>
2016-04-24 09:46:22 +0200

    Updated the Date Format for displaying... dates... so I like it better now.

--- lod_django/league_of_designs/settings.py
+++ lod_django/league_of_designs/settings.py
@@ -104,10 +104,11 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
+DATETIME_FORMAT = "l jS F Y, H:i"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
