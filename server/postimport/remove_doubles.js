8bd9a2e869ee99a78ea6df4ef828bcc24e57ae3d
Artemys <jacques.gegout@telecomnancy.net>
2017-01-28 15:04:18 +0100

    - Updated Warwick assets. - Will fix LoD Postimport's Counters later.

--- server/postimport/remove_doubles.js
+++ server/postimport/remove_doubles.js
@@ -1,11 +1,12 @@
-// Normally you don't need this but just in case (for instance after you update to the new API with config control).
+var n_duplicates = 0;
 
-db.mr_reds.find().forEach(function(red) {
-    var cur = db.mr_reds.find({'post_id': red['post_id']});
-    var first = true;
-    while (cur.hasNext()) {
-        var doc = cur.next();
-        if (first) {first = false; continue;}
-        db.mr_reds.remove({ _id: doc._id });
-    }
-});
+db.reds.aggregate([
+	{"$group" : { "_id": "$post_id", "count": { "$sum": 1 }, dups: { "$addToSet": "$_id" }, } },
+	{"$match": {"_id" :{ "$ne" : null } , "count" : {"$gt": 1} } },
+]).forEach(function(doc) {
+	n_duplicates = n_duplicates + 1;
+    doc.dups.shift();      // First element skipped for deleting
+    db.reds.remove({_id : {$in: doc.dups }});  // Delete remaining duplicates
+})
+
+print("Removed : " + n_duplicates)
